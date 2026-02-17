cd7ee5be804b6c9a4fb78ee7596452767ccd88ed
Bryan Cunningham <bryan.cunningham@me.com>
2026-02-17 13:37:56 -0500

    restructure chips folders

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -12,7 +12,7 @@ export * from "./button";
 export * from "./callout";
 export * from "./card";
 export * from "./checkbox";
-export * from "./chip";
+export * from "./chips";
 export * from "./color-password";
 export * from "./container";
 export * from "./copy-click";
